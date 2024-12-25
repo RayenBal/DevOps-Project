2c42e25054cde2df505443f8eac551b500f28d14
Mohamed Rayen Balghouthi <80216457+RayenBal@users.noreply.github.com>
2024-12-25 12:18:34 +0100

    Update run_security_smoke_tests.sh

--- run_security_smoke_tests.sh
+++ run_security_smoke_tests.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-
-DB_HOST="localhost"
-DB_USER="user"
-DB_PASS="password"
-DB_NAME="mydatabase"
+# Set the database details
+DB_HOST="devsecops-mysqldb-1"  # Name of the MySQL container
+DB_USER="root"  # Use root or the correct user
+DB_PASS="root"  # Ensure this matches your MySQL root password
+DB_NAME="ski-project3"  # Replace with your actual database name
 
 # Try to connect to the database and list tables
 if mysql -h $DB_HOST -u $DB_USER -p$DB_PASS -e "SHOW TABLES;" $DB_NAME; then
